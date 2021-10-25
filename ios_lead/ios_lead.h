752429c8a4181aae5ce62d379d218e46a53251f6
rajeshyadav7075 <rajurajeshbanka2@gmail.com>
2021-10-25 19:24:23 +0530

    Initial Commit

---
+++ ios_lead/ios_lead.h
@@ -0,0 +1,18 @@
+//
+//  ios_lead.h
+//  ios_lead
+//
+//  Created by Rajesh on 25/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ios_lead.
+FOUNDATION_EXPORT double ios_leadVersionNumber;
+
+//! Project version string for ios_lead.
+FOUNDATION_EXPORT const unsigned char ios_leadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_lead/PublicHeader.h>
+
+
